617f43ff114a2d7eafc8ff57bf34979be717bad2
Vitaliy <modecom.pirogenko@gmail.com>
2023-11-16 13:12:02 +0300

    Initial commit

---
+++ store_authorization/src/main/java/com/example/store_authorization/StoreAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.store_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreAuthorizationApplication.class, args);
+    }
+
+}
