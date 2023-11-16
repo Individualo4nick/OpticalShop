e59e2b2ce8a0194b200d766ab0b36c5f5e7fad3c
Vitaliy <modecom.pirogenko@gmail.com>
2023-11-16 13:11:31 +0300

    Initial commit

---
+++ optical_shop/src/test/java/com/example/optical_shop/OpticalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optical_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpticalShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
