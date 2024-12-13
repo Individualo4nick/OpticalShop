30d52f7029feab26c1cc5ca2b5a5a8bf0ffd312e
Vitaliy <modecom.pirogenko@gmail.com>
2024-12-14 01:08:24 +0300

    поправлены конфиг файлы

--- store_authorization/src/main/java/com/example/store_authorization/StoreAuthorizationApplication.java
+++ store_authorization/src/main/java/com/example/store_authorization/StoreAuthorizationApplication.java
@@ -1,6 +1,5 @@
 package com.example.store_authorization;
 
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
